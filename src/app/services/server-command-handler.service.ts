21c4b77e8b2247e2460a3ed38bcf68d9c47d8f16
Enisz Abdalla <enisz87@gmail.com>
2026-04-20 23:49:51 +0200

    #35: Fix RxJS import path in ServerCommandHandlerService

--- src/app/services/server-command-handler.service.ts
+++ src/app/services/server-command-handler.service.ts
@@ -1,7 +1,6 @@
 import { DestroyRef, inject, Injectable } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { EMPTY, from } from 'rxjs';
-import { catchError, concatMap, filter } from 'rxjs/operators';
+import { catchError, concatMap, EMPTY, filter, from } from 'rxjs';
 import { AppCommand, ServerCommand } from '../models/command.model';
 import { CommandBusService } from './command-bus.service';
 import { ServerStoreService } from './server-store.service';
