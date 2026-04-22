dd2ff8f158e82557ccf869406f47dc9f37331db2
Enisz Abdalla <enisz87@gmail.com>
2026-04-22 19:22:24 +0200

    #35: add SettingsStateService for centralised settings dirty tracking

---
+++ src/app/pages/settings/settings-state.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, computed, signal } from '@angular/core';
+import { SettingsTabId } from './settings.interface';
+
+type DirtyMap = Record<SettingsTabId, boolean>;
+
+const INITIAL_DIRTY: DirtyMap = {
+  general: false,
+  server: false,
+  'torrent-list-grid': false,
+  'status-bar': false,
+};
+
+@Injectable()
+export class SettingsStateService {
+  private readonly dirtyTabs = signal<DirtyMap>({ ...INITIAL_DIRTY });
+  private readonly saveFns = new Map<SettingsTabId, () => Promise<void>>();
+
+  public readonly isDirty = computed(() => Object.values(this.dirtyTabs()).some(Boolean));
+  public readonly isDirtyMap = computed(() => this.dirtyTabs());
+
+  public markDirty(id: SettingsTabId, dirty: boolean): void {
+    this.dirtyTabs.update((tabs) => ({ ...tabs, [id]: dirty }));
+  }
+
+  public registerSave(id: SettingsTabId, fn: () => Promise<void>): void {
+    this.saveFns.set(id, fn);
+  }
+
+  public resetDirty(): void {
+    this.dirtyTabs.set({ ...INITIAL_DIRTY });
+  }
+
+  public async saveAll(): Promise<void> {
+    const dirty = this.dirtyTabs();
+    await Promise.all(
+      (Object.keys(dirty) as SettingsTabId[])
+        .filter((id) => dirty[id])
+        .map((id) => this.saveFns.get(id)?.() ?? Promise.resolve()),
+    );
+    this.resetDirty();
+  }
+}
