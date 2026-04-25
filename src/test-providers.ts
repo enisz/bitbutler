aceedb05546d5c40527d33bddd39de30e8add5b8
Enisz Abdalla <enisz87@gmail.com>
2026-04-25 14:48:02 +0200

    #49: add global TranslateService and FilesizePipe providers for tests

--- src/test-providers.ts
+++ src/test-providers.ts
@@ -1,3 +1,5 @@
 import { provideZonelessChangeDetection } from '@angular/core';
+import { provideTranslateService } from '@ngx-translate/core';
+import { FilesizePipe } from './app/pipes/filesize-pipe';
 
-export default [provideZonelessChangeDetection()];
+export default [provideZonelessChangeDetection(), provideTranslateService(), FilesizePipe];
