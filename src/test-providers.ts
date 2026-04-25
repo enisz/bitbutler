e27b90b55858882b97e495da57daa7d766842c47
Enisz Abdalla <enisz87@gmail.com>
2026-04-25 15:10:49 +0200

    #49: fix import order in test-providers.ts

--- src/test-providers.ts
+++ src/test-providers.ts
@@ -1,7 +1,7 @@
 import { provideZonelessChangeDetection } from '@angular/core';
-import { HumanizeDurationPipe } from './app/pipes/humanize-duration-pipe';
 import { provideTranslateService } from '@ngx-translate/core';
 import { FilesizePipe } from './app/pipes/filesize-pipe';
+import { HumanizeDurationPipe } from './app/pipes/humanize-duration-pipe';
 
 export default [
   provideZonelessChangeDetection(),
