aca9414cb1bcaab2fcc3bc89a2f836708ff1d7b4
Enisz Abdalla <33585488+enisz@users.noreply.github.com>
2026-06-14 18:44:43 +0200

    #159: added appendTo input

--- packages/app/src/app/components/category-select/category-select.ts
+++ packages/app/src/app/components/category-select/category-select.ts
@@ -54,6 +54,7 @@ export class CategorySelect implements ControlValueAccessor {
   private readonly qbService = inject(QbService);
 
   public readonly icons = { faTriangleExclamation };
+  public readonly appendTo = input('');
 
   public categories = signal<string[]>([]);
   public selectControl = new FormControl('');
