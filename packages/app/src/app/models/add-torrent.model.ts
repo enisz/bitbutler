b2d1e6a84a91b9490f2ce8a34f5e016e3d4f6250
Abdalla Enisz <enisz87@gmail.com>
2026-06-14 02:59:40 +0200

    #159: add AddTorrentFormGroup type

--- packages/app/src/app/models/add-torrent.model.ts
+++ packages/app/src/app/models/add-torrent.model.ts
@@ -1,3 +1,4 @@
+import { FormControl, FormGroup } from '@angular/forms';
 import { ShareLimitValue } from '../components/share-limit/share-limit';
 import { TransferLimitValue } from '../components/transfer-limit/transfer-limit';
 
@@ -30,3 +31,20 @@ export const DEFAULT_ADD_TORRENT_SETTINGS: AddTorrentSettings = {
   transferRateLimits: null,
   shareLimits: { ratioLimit: -2, seedingTimeLimit: -2, inactiveSeedingTimeLimit: -2 },
 };
+
+export type AddTorrentFormGroup = FormGroup<{
+  file: FormControl<string>;
+  magnetLinks: FormControl<string>;
+  savepath: FormControl<string | null>;
+  rename: FormControl<string | null>;
+  paused: FormControl<boolean>;
+  category: FormControl<string | null>;
+  root_folder: FormControl<RootFolderMode>;
+  tags: FormControl<string[] | null>;
+  skip_checking: FormControl<boolean>;
+  sequentialDownload: FormControl<boolean>;
+  firstLastPiecePrio: FormControl<boolean>;
+  transferRateLimits: FormControl<TransferLimitValue | null>;
+  shareLimits: FormControl<ShareLimitValue | null>;
+  autoTMM: FormControl<boolean>;
+}>;
