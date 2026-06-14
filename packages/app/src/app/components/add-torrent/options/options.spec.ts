f53053fde2801079b775a5b5bc41d0bb731a7566
Abdalla Enisz <enisz87@gmail.com>
2026-06-14 03:32:39 +0200

    #159: add AddTorrentOptions tab component

---
+++ packages/app/src/app/components/add-torrent/options/options.spec.ts
@@ -0,0 +1,93 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AddTorrentFormGroup, RootFolderMode } from '../../../models/add-torrent.model';
+import { ShareLimitValue } from '../../share-limit/share-limit';
+import { TransferLimitValue } from '../../transfer-limit/transfer-limit';
+import { AddTorrentOptions } from './options';
+
+function createForm(): AddTorrentFormGroup {
+  return new FormGroup({
+    file: new FormControl<string>('', { nonNullable: true }),
+    magnetLinks: new FormControl<string>('', { nonNullable: true }),
+    savepath: new FormControl<string | null>(null),
+    rename: new FormControl<string | null>(null),
+    paused: new FormControl<boolean>(false, { nonNullable: true }),
+    category: new FormControl<string | null>(null),
+    root_folder: new FormControl<RootFolderMode>('unset', { nonNullable: true }),
+    tags: new FormControl<string[] | null>(null),
+    skip_checking: new FormControl<boolean>(false, { nonNullable: true }),
+    sequentialDownload: new FormControl<boolean>(false, { nonNullable: true }),
+    firstLastPiecePrio: new FormControl<boolean>(false, { nonNullable: true }),
+    transferRateLimits: new FormControl<TransferLimitValue | null>(null),
+    shareLimits: new FormControl<ShareLimitValue | null>(null),
+    autoTMM: new FormControl<boolean>(false, { nonNullable: true }),
+  });
+}
+
+describe('AddTorrentOptions', () => {
+  let component: AddTorrentOptions;
+  let fixture: ComponentFixture<AddTorrentOptions>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddTorrentOptions],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AddTorrentOptions);
+    component = fixture.componentInstance;
+    fixture.componentRef.setInput('form', createForm());
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should expose three root folder options', () => {
+    expect(component.rootFolderOptions.map((option) => option.value)).toEqual([
+      'unset',
+      'true',
+      'false',
+    ]);
+  });
+
+  it('should toggle skip_checking via the form', () => {
+    const checkbox: HTMLInputElement = fixture.nativeElement.querySelector('#skip_checking');
+    checkbox.click();
+    fixture.detectChanges();
+
+    expect(component.form().controls.skip_checking.value).toBe(true);
+  });
+
+  it('should toggle paused via the form', () => {
+    const checkbox: HTMLInputElement = fixture.nativeElement.querySelector('#paused');
+    checkbox.click();
+    fixture.detectChanges();
+
+    expect(component.form().controls.paused.value).toBe(true);
+  });
+
+  it('should toggle autoTMM via the form', () => {
+    const checkbox: HTMLInputElement = fixture.nativeElement.querySelector('#autoTMM');
+    checkbox.click();
+    fixture.detectChanges();
+
+    expect(component.form().controls.autoTMM.value).toBe(true);
+  });
+
+  it('should toggle sequentialDownload via the form', () => {
+    const checkbox: HTMLInputElement = fixture.nativeElement.querySelector('#sequentialDownload');
+    checkbox.click();
+    fixture.detectChanges();
+
+    expect(component.form().controls.sequentialDownload.value).toBe(true);
+  });
+
+  it('should toggle firstLastPiecePrio via the form', () => {
+    const checkbox: HTMLInputElement = fixture.nativeElement.querySelector('#firstLastPiecePrio');
+    checkbox.click();
+    fixture.detectChanges();
+
+    expect(component.form().controls.firstLastPiecePrio.value).toBe(true);
+  });
+});
---
+++ packages/app/src/app/components/add-torrent/options/options.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { AddTorrentFormGroup } from '../../../models/add-torrent.model';
+import { BbPopover } from '../../bb-popover/bb-popover';
+
+@Component({
+  selector: 'app-add-torrent-options',
+  imports: [ReactiveFormsModule, NgSelectModule, FontAwesomeModule, BbPopover, TranslatePipe],
+  templateUrl: './options.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AddTorrentOptions {
+  private readonly translateService = inject(TranslateService);
+
+  public form = input.required<AddTorrentFormGroup>();
+
+  public faExclamationTriangle = faExclamationTriangle;
+
+  public rootFolderOptions = [
+    {
+      value: 'unset',
+      label: this.translateService.instant(
+        'components.add-torrent.add-form.root-folder.option.default',
+      ),
+    },
+    {
+      value: 'true',
+      label: this.translateService.instant(
+        'components.add-torrent.add-form.root-folder.option.create-root-folder',
+      ),
+    },
+    {
+      value: 'false',
+      label: this.translateService.instant(
+        'components.add-torrent.add-form.root-folder.option.do-not-create-root-folder',
+      ),
+    },
+  ];
+}
