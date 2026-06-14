a02b26179232ceccc055ce9a4cdbb75d54f50be7
Abdalla Enisz <enisz87@gmail.com>
2026-06-14 03:14:45 +0200

    #159: add AddTorrentGeneral tab component

---
+++ packages/app/src/app/components/add-torrent/general/general.ts
@@ -0,0 +1,41 @@
+import { ChangeDetectionStrategy, Component, input, output, viewChild } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFile, faLink } from '@fortawesome/free-solid-svg-icons';
+import { TranslatePipe } from '@ngx-translate/core';
+import { AutofocusDirective } from '../../../directives/autofocus';
+import { AddTorrentFormGroup } from '../../../models/add-torrent.model';
+import { BbPopover } from '../../bb-popover/bb-popover';
+import { CategorySelect } from '../../category-select/category-select';
+import { SavePathSelect } from '../../save-path-select/save-path-select';
+import { TagSelect } from '../../tag-select/tag-select';
+
+@Component({
+  selector: 'app-add-torrent-general',
+  imports: [
+    ReactiveFormsModule,
+    AutofocusDirective,
+    FontAwesomeModule,
+    BbPopover,
+    TranslatePipe,
+    CategorySelect,
+    SavePathSelect,
+    TagSelect,
+  ],
+  templateUrl: './general.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AddTorrentGeneral {
+  public form = input.required<AddTorrentFormGroup>();
+  public inputMode = input.required<'file' | 'link'>();
+  public inputModeChange = output<'file' | 'link'>();
+  public fileSelected = output<Event>();
+
+  private readonly categorySelect = viewChild(CategorySelect);
+
+  public icons = { faFile, faLink };
+
+  public ensureCategoryExists(): Promise<boolean> | undefined {
+    return this.categorySelect()?.ensureCategoryExists();
+  }
+}
