55ee270ea89600e2421bf5e86ef8c98e7c159026
Enisz Abdalla <enisz87@gmail.com>
2026-08-01 00:16:53 +0200

    #251: register all-time widgets in status bar settings

--- packages/app/src/app/models/status-bar-settings.model.ts
+++ packages/app/src/app/models/status-bar-settings.model.ts
@@ -5,7 +5,7 @@ export interface StatusBarSettings {
 }
 
 export const DEFAULT_STATUS_BAR_SETTINGS: StatusBarSettings = {
-  available: ['selection'],
+  available: ['selection', 'alltime-down', 'alltime-up', 'alltime-ratio'],
   left: ['connection-status', 'nodes', 'ratio', 'global-down', 'global-up'],
   right: ['download-speed', 'upload-speed', 'free-space', 'polling-indicator'],
 };
