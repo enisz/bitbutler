d38e652bfe1ba7637ddfb6f936d92ca677e6be21
Enisz Abdalla <enisz87@gmail.com>
2026-08-19 22:03:09 +0200

    Fix memory leak in CommandBusService by implementing rxjs-interop patterns

--- packages/app/src/app/services/command-bus.service.ts
+++ packages/app/src/app/services/command-bus.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, DestroyRef } from '@angular/core';
 import { Subject } from 'rxjs';
+import { takeUntilDestroyed } from 'rxjs-interop';
 import { AppCommand } from '../models/command.model';
 
 @Injectable({ providedIn: 'root' })
 export class CommandBusService {
   private readonly _commands = new Subject<AppCommand>();
-  readonly commands$ = this._commands.asObservable();
+  readonly commands$ = this._commands.asObservable().pipe(
+    takeUntilDestroyed()
+  );
+
+  constructor(private readonly destroyRef: DestroyRef) {}
 
   emit(cmd: AppCommand) {
     this._commands.next(cmd);
